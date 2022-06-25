02ef98400ac0c1ea1d48c8d342ef6e9a377d363f
Rajiv Toora <rajiv.toora@gmail.com>
2022-06-25 12:49:48 -0500

    Add Test project
    Adding base test project for Api web project.

---
+++ test/Web/RToora.DemoApi.Web.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace RToora.DemoApi.Web.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        int n = 1;
+
+        // Act
+
+        // Assert
+        Assert.Equal(1, n);
+    }
+}
